771e247a294868806f2a99de145e3fc68b9dc5b4
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2017-03-25 15:18:08 +0200

    Extracted service interfaces

---
+++ ASE.ScienceDirectExtractor/ASE.ScienceDirectExtractor.Console/Services/IAuthorSearchService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ASE.ScienceDirectExtractor.SearchAuthorApi;
+
+namespace ASE.ScienceDirectExtractor.Console.Services
+{
+	public interface IAuthorSearchService
+	{
+		Task<SearchAuthorApiResult> SearchAuthorAsync(string firstName, string lastName);
+	}
+}
---
+++ ASE.ScienceDirectExtractor/ASE.ScienceDirectExtractor.Console/Services/IPublicationSearchService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ASE.ScienceDirectExtractor.ElsevierSearchApi;
+
+namespace ASE.ScienceDirectExtractor.Console.Services
+{
+	public interface IPublicationSearchService
+	{
+		Task<ScienceDirectSearchResult> GetPublicationsAsync(string keyWord, int entryNumber);
+	}
+}
